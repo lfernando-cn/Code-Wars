dd4613c54be64dd3b7cd9eea872caba692ade946
lfernando-cn <luisfer_cn@hotmail.com>
2023-11-04 18:36:28 -0300

    #2

---
+++ challenge2/src/App.java
@@ -0,0 +1,14 @@
+/*We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100" */
+
+class Kata {
+  public static String numberToString(int num) {
+    return String.valueOf(num); //The method String.valueOf() converts any primitive-type value into a string object.
+  }
+}
